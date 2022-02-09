5d2bd37e38450bda9c46426a415117f589b39759
ekankam <eb.kankam26@gmail.com>
2022-02-09 12:17:05 -0600

    Added footer and applied styling. Created a function to get the current footer date.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#mobile-menu')
 const hamburgerMenu = document.querySelector('#hamburger-menu')
 const closeIcon = document.querySelector('#close-icon')
+const footerDate = document.querySelector('#footer-date')
 
 hamburgerMenu.addEventListener('click', () => {
     menu.classList.add('show')
@@ -9,3 +10,10 @@ hamburgerMenu.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     menu.classList.remove('show')
 })
+
+function getCurrentFooterDate() {
+    const currentYear = new Date().getFullYear()
+    footerDate.innerHTML = `&copy; ${currentYear} Loopstudios. All rights reserved.`
+}
+
+getCurrentFooterDate()
